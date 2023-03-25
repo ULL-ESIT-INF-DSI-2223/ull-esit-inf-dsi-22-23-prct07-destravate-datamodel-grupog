5170988cf6324a18b882edfa4e61d2a1a7633aa0
Lucas Pérez <alu0101399928@ull.edu.es>
2023-03-25 18:07:16 +0000

    Cambios en el challenge prompter

--- src/cli/managers/friends_manager.ts
+++ src/cli/managers/friends_manager.ts
@@ -1,1 +1,25 @@
+import inquirer from "inquirer";
+import Database from "../../db/database.js";
+import { users } from "../choices.js";
+import SessionManager from "./session_manager.js";
 
+export default class FriendsManager {
+  constructor(private db: Database, private session: SessionManager) {}
+  async edit(): Promise<void> {
+    const uid = this.session.currentUserID();
+    if (!uid) {
+      return;
+    }
+
+    const question = await inquirer.prompt([
+      {
+        type: "checkbox",
+        name: "friends",
+        message: `Selecione los amigos a añadir o eliminar:`,
+        choices: users(this.db),
+      },
+    ]);
+
+    const input = await inquirer.prompt(question);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import ChallengePrompter from "./cli/challenge_prompter.js";
 import Database from "./db/database.js";
 
 const db = new Database();
 await db.load();
+
+new ChallengePrompter(db).add();
