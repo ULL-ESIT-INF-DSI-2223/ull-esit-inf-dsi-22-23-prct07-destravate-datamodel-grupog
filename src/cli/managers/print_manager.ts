2114ee5e3dea466f7f4d19593871ba45b65ab929
AlejandroGarciaBautista <alu0101387604@ull.edu.es>
2023-03-26 15:23:12 +0000

    Minor Changes testing

--- src/cli/managers/print_manager.ts
+++ src/cli/managers/print_manager.ts
@@ -5,24 +5,45 @@ import Route from "../../route/route.js";
 import User from "../../user/user.js";
 import { getBorderCharacters, table } from "table";
 
+/**
+ * Class made to manage all the functionalities in which something must be printed on the screen
+ */
 export default class PrintManager {
+  /**
+   * Constructor of the manager
+   * @param db 
+   * @param session 
+   */
   constructor(private db: Database, private session: SessionManager) {}
 
+  /**
+   * Method to print all the groups on the system
+   */
   printGroups() {
     const groups = this.db.groups().slice();
     Group.printTableLessInfo(groups);
   }
 
+  /**
+   * Method to print all the routes on the system
+   */
   printRoutes() {
     const routes = this.db.routes().slice();
     Route.printTable(routes);
   }
 
+  /**
+   * Method to print all the users on the system
+   */
   printUsers() {
     const users = this.db.users().slice();
     User.printTableLessInfo(users);
   }
 
+  /**
+   * Method to print from each group the TOP 3 user
+   * using the km acumulated.
+   */
   printTop3UsersKm() {
     const groups = this.db.groups().slice();
     groups.forEach((group) => {
@@ -49,6 +70,10 @@ export default class PrintManager {
     })
   }
 
+  /**
+   * Method to print from each group the TOP 3 user
+   * using the slope acumulated.
+   */
   printTop3UsersSlope() {
     const groups = this.db.groups().slice();
     groups.forEach((group) => {
