2bb088ba093d5f946fa89d42a74218dd4d62dfe7
Alejandro García <alu0101387604@ull.edu.es>
2023-03-26 20:39:04 +0100

    Update sort_funct testing

--- tests/utils/sort_func.spec.ts
+++ tests/utils/sort_func.spec.ts
@@ -2,15 +2,19 @@ import { expect } from "chai";
 import { compareStrings, compareStringsIgnoreCase, compareStringsFirstIgnoringCase } from "../../src/utils/sort_func.js"
 
 describe("Sorting functions", () => {
-  // it("compareString with", () => {
-  //   expect(compareStrings()).to.be.eql()
-  // });
+  it("compareString with hello and Joshep", () => {
+    expect(compareStrings("AntoNio", "MichaEL")).to.be.eql(-1)
+    expect(compareStrings("MichaEL", "AntoNio")).to.be.eql(1)
+  });
 
-  // it("compareStringIgnoreCase with", () => {
-  //   expect(compareStringsIgnoreCase()).to.be.eql()
-  // });
+  it("compareStringIgnoreCase with AntoNio and MichaEL", () => {
+    expect(compareStringsIgnoreCase("AntoNio", "MichaEL")).to.be.eql(-1)
+    expect(compareStringsIgnoreCase("MichaEL", "AntoNio")).to.be.eql(1)
+  });
 
-  // it("compareStringsFirstIgnoringCase with", () => {
-  //   expect(compareStringsFirstIgnoringCase()).to.be.eql()
-  // });
+  it("compareStringsFirstIgnoringCase with", () => {
+    expect(compareStringsFirstIgnoringCase("AntoNio", "MichaEL")).to.be.eql(-1)
+    expect(compareStringsFirstIgnoringCase("MichaEL", "AntoNio")).to.be.eql(1)
+    expect(compareStringsFirstIgnoringCase("michael", "michael")).to.be.eql(0)
+  });
 });
